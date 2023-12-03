27036a91bb381ab8bf8f2f2438c6b60886662e59
javillo-ur <55446633+javillo-ur@users.noreply.github.com>
2023-12-03 19:07:27 +0100

    True one line

--- Day1Part2/Program.cs
+++ Day1Part2/Program.cs
@@ -1,3 +1,1 @@
-﻿Console.WriteLine(File.ReadAllLines("..\\..\\..\\input.txt")
-    .Select(j => j.Replace("one", "o1e").Replace("two", "t2o").Replace("three", "t3ree").Replace("four","f4ur").Replace("five", "f5ve").Replace("six", "s6x").Replace("seven", "s7ven").Replace("eight", "e8ght").Replace("nine", "n9ne"))
-    .Sum(j => int.Parse(j.First(c => char.IsDigit(c)) + "") * 10 + int.Parse(j.Last(c => char.IsDigit(c)) + "")));
+﻿Console.WriteLine(File.ReadAllLines("input.txt").Select(j => j.Replace("one", "o1e").Replace("two", "t2o").Replace("three", "t3ree").Replace("four","f4ur").Replace("five", "f5ve").Replace("six", "s6x").Replace("seven", "s7ven").Replace("eight", "e8ght").Replace("nine", "n9ne")).Sum(j => int.Parse(j.First(c => char.IsDigit(c)) + "") * 10 + int.Parse(j.Last(c => char.IsDigit(c)) + "")));
--- Day1Puzzle1/Program.cs
+++ Day1Puzzle1/Program.cs
@@ -1,2 +1,1 @@
-﻿Console.WriteLine(File.ReadAllLines("input.txt")
-    .Sum(j => int.Parse(j.First(c => char.IsDigit(c)) + "") * 10 + int.Parse(j.Last(c => char.IsDigit(c)) + "")));
+﻿Console.WriteLine(File.ReadAllLines("input.txt").Sum(j => int.Parse(j.First(c => char.IsDigit(c)) + "") * 10 + int.Parse(j.Last(c => char.IsDigit(c)) + "")));
